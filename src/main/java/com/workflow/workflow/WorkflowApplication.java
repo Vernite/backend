0e7d30560b282767cbbf59867a45517527eb3224
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-04-13 17:53:10 +0200

    WFLOW-71 - allow cors

--- src/main/java/com/workflow/workflow/WorkflowApplication.java
+++ src/main/java/com/workflow/workflow/WorkflowApplication.java
@@ -2,6 +2,9 @@ package com.workflow.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
@@ -11,5 +14,15 @@ import io.swagger.v3.oas.annotations.servers.Server;
 public class WorkflowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkflowApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://127.0.0.1:4200");
+			}
+		};
 	}
 }
