234612cb3f91832930bf9cbc70ff1eb14841f561
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-04-18 18:41:01 +0200

    WFLOW-112 - created placeholder user class

---
+++ src/main/java/com/workflow/workflow/user/User.java
@@ -0,0 +1,86 @@
+package com.workflow.workflow.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class User {
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String email;
+    private String imagePath;
+    @JsonIgnore
+    private String password;
+
+    User() {}
+
+    User(String name, String surname, String username, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/workflow/workflow/user/UserRepository.java
@@ -0,0 +1,5 @@
+package com.workflow.workflow.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {}
