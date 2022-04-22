dd2a04ba61d2f54d0b1835ef3b7584d75168696d
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-04-22 22:16:48 +0200

    WFLOW-94 - project requet adjustments

--- src/main/java/com/workflow/workflow/project/ProjectRequest.java
+++ src/main/java/com/workflow/workflow/project/ProjectRequest.java
@@ -2,18 +2,28 @@ package com.workflow.workflow.project;
 
 public class ProjectRequest {
     private String name;
+    private Long workspaceId;
 
     public ProjectRequest() {}
 
-    public ProjectRequest(String name) {
+    public ProjectRequest(String name, Long workspaceId) {
         this.name = name;
+        this.workspaceId = workspaceId;
     }
 
     public String getName() {
         return name;
     }
+
+    public Long getWorkspaceId() {
+        return workspaceId;
+    }
     
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setWorkspaceId(Long workspaceId) {
+        this.workspaceId = workspaceId;
     }
 }
