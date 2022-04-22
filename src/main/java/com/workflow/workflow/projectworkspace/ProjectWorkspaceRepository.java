a5e195fa675c19722567b08d2a313744a6e24a26
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-04-22 21:19:23 +0200

    WFLOW-114 - added repository for pivot table

---
+++ src/main/java/com/workflow/workflow/projectworkspace/ProjectWorkspaceRepository.java
@@ -0,0 +1,5 @@
+package com.workflow.workflow.projectworkspace;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectWorkspaceRepository extends CrudRepository<ProjectWorkspace, ProjectWorkspaceKey> {}
