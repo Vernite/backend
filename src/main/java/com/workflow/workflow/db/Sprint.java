7d79792148b7e8ca0d15dc7723ffb2c0cbfc3c5b
Adrian Antkowiak <adiantek@gmail.com>
2022-04-23 19:57:25 +0200

    add Sprint & User models

---
+++ src/main/java/com/workflow/workflow/db/Sprint.java
@@ -0,0 +1,72 @@
+package com.workflow.workflow.db;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.workflow.workflow.project.Project;
+
+@Entity
+public class Sprint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    private Date startDate;
+    private Date finishDate;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", foreignKey = @ForeignKey(name = "fk_sprint_project"))
+    private Project project;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
--- src/main/java/com/workflow/workflow/user/User.java
+++ src/main/java/com/workflow/workflow/user/User.java
@@ -1,86 +1,138 @@
 package com.workflow.workflow.user;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class User {
-    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
-    private String name;
-    private String surname;
-    private String username;
-    private String email;
-    private String imagePath;
-    @JsonIgnore
-    private String password;
 
-    public User() {}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @JsonIgnore
+    @Column(nullable = false, length = 32)
+    @NotNull
+    private byte[] hash;
+
+    @JsonIgnore
+    @Column(nullable = false, length = 20)
+    private byte[] salt;
+
+    private String avatar;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String username;
+
+    public User() {
+    }
 
     public User(String name, String surname, String username, String email, String password) {
-        this.name = name;
-        this.surname = surname;
-        this.username = username;
-        this.email = email;
-        this.password = password;
+        this.setName(name);
+        this.setSurname(surname);
+        this.setUsername(username);
+        this.setEmail(email);
+        this.setPassword(password);
     }
-    
+
     public Long getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
+    public byte[] getHash() {
+        return hash;
+    }
+
+    public void setHash(byte[] hash) {
+        this.hash = hash;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        byte[] salt = new byte[20];
+        new SecureRandom().nextBytes(salt);
+        byte[] hash;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update(password.getBytes(StandardCharsets.UTF_8));
+            digest.update(salt);
+            hash = digest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        this.setHash(hash);
+        this.setSalt(salt);
     }
+
 }
