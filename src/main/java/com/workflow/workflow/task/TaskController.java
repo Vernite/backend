fe1e05a6d3a025b4770b37d491f124cb086e4462
Adrian Antkowiak <adiantek@gmail.com>
2022-05-05 19:11:13 +0200

    add listing tasks

--- src/main/java/com/workflow/workflow/task/TaskController.java
+++ src/main/java/com/workflow/workflow/task/TaskController.java
@@ -1,7 +1,11 @@
 package com.workflow.workflow.task;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import com.workflow.workflow.project.Project;
+import com.workflow.workflow.project.ProjectRepository;
 import com.workflow.workflow.status.Status;
 import com.workflow.workflow.status.StatusRepository;
 import com.workflow.workflow.user.UserRepository;
@@ -9,6 +13,7 @@ import com.workflow.workflow.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -29,6 +35,7 @@ public class TaskController {
 
     private static final String TASK_NOT_FOUND = "task not found";
     private static final String STATUS_NOT_FOUND = "status not found";
+    private static final String PROJECT_NOT_FOUND = "project not found";
     private static final String STATUS_AND_PROJECT_NOT_RELATION = "status is not in relation with given project";
 
     @Autowired
@@ -37,6 +44,27 @@ public class TaskController {
     private StatusRepository statusRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Operation(summary = "Get all tasks.", description = "This method returns array of all tasks for project with given ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of all tasks. Can be empty.", content = {
+                    @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Task.class)))
+            }),
+            @ApiResponse(responseCode = "404", description = "Project with given ID not found.", content = @Content())
+    })
+    @GetMapping("/")
+    public Iterable<Task> all(@PathVariable long projectId) {
+        Project project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, PROJECT_NOT_FOUND));
+        List<Task> tasks = new ArrayList<>();
+        for (Status status : project.getStatuses()) {
+            tasks.addAll(this.taskRepository.findByStatus(status));
+        }
+        tasks.sort((a, b) -> Long.compare(a.getId(), b.getId()));
+        return tasks;
+    }
 
     @Operation(summary = "Create task.", description = "This method creates new task. On success returns newly created task.")
     @ApiResponses(value = {
