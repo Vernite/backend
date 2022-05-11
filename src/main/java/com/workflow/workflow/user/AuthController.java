e9043b51513088a402c4e84f1f7b7f63e32df50a
Adrian Antkowiak <adiantek@gmail.com>
2022-05-11 10:46:57 +0200

    WFLOW-148 - register account

---
+++ src/main/java/com/workflow/workflow/user/AuthController.java
@@ -0,0 +1,63 @@
+package com.workflow.workflow.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Operation(summary = "Register account.", description = "This method registers a new account. On success returns newly created user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Newly created user.", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Some fields are missing.", content = @Content()),
+            @ApiResponse(responseCode = "422", description = "Username or email is already taken.", content = @Content())
+    })
+    @PostMapping("/register")
+    public User register(@RequestBody RegisterRequest req) {
+        if (req.getEmail() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing email");
+        }
+        if (req.getName() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing name");
+        }
+        if (req.getPassword() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing password");
+        }
+        if (req.getSurname() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing surname");
+        }
+        if (req.getUsername() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing username");
+        }
+        if (userRepository.findByUsername(req.getUsername()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "this username is already taken");
+        }
+        if (userRepository.findByEmail(req.getEmail()).isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "this email is already taken");
+        }
+        User u = new User();
+        u.setEmail(req.getEmail());
+        u.setName(req.getName());
+        u.setPassword(req.getPassword());
+        u.setSurname(req.getSurname());
+        u.setUsername(req.getUsername());
+        return userRepository.save(u);
+    }
+}
---
+++ src/main/java/com/workflow/workflow/user/RegisterRequest.java
@@ -0,0 +1,49 @@
+package com.workflow.workflow.user;
+
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private String username;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
