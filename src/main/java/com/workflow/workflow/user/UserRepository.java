4b30b38dd0cccaa1f6f5169f5a9da837b105f0d3
Adrian Antkowiak <adiantek@gmail.com>
2022-05-13 04:37:24 +0200

    WFLOW-191 - logging in

--- src/main/java/com/workflow/workflow/user/UserRepository.java
+++ src/main/java/com/workflow/workflow/user/UserRepository.java
@@ -1,10 +1,8 @@
 package com.workflow.workflow.user;
 
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
+    User findByEmail(String email);
+    User findByUsername(String username);
 }
