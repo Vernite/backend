7fe75d2bf7fa7618db47cce5b9307527335d688e
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-05-24 18:18:27 +0200

    Remove unused repository functions

--- src/main/java/com/workflow/workflow/projectworkspace/ProjectWorkspaceRepository.java
+++ src/main/java/com/workflow/workflow/projectworkspace/ProjectWorkspaceRepository.java
@@ -3,19 +3,10 @@ package com.workflow.workflow.projectworkspace;
 import java.util.List;
 
 import com.workflow.workflow.project.Project;
-import com.workflow.workflow.workspace.Workspace;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProjectWorkspaceRepository extends CrudRepository<ProjectWorkspace, ProjectWorkspaceKey> {
-    /**
-     * This methods finds all pivot table entry for given workspace.
-     * 
-     * @param workspace - workspace which pivot entries will be returned.
-     * @return Pivot table entries list for workspace.
-     */
-    List<ProjectWorkspace> findByWorkspace(Workspace workspace);
-
     /**
      * It just works.
      * 
--- src/main/java/com/workflow/workflow/workspace/WorkspaceRepository.java
+++ src/main/java/com/workflow/workflow/workspace/WorkspaceRepository.java
@@ -1,27 +1,6 @@
 package com.workflow.workflow.workspace;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.workflow.workflow.user.User;
 import com.workflow.workflow.utils.SoftDeleteRepository;
 
 public interface WorkspaceRepository extends SoftDeleteRepository<Workspace, WorkspaceKey> {
-    /**
-     * This method finds workspaces for given user.
-     * 
-     * @param user - user which workspaces wil be returned.
-     * @return List object with all workspaces for given user.
-     */
-    List<Workspace> findByUser(User user);
-
-    /**
-     * This method looks for workspace with given id and belonging to given user.
-     * 
-     * @param id   - id of workspace.
-     * @param user - user which workspace is being sought.
-     * @return workspace with given id belonging to given user or nothing when not
-     *         found.
-     */
-    Optional<Workspace> findByIdAndUser(WorkspaceKey id, User user);
 }
