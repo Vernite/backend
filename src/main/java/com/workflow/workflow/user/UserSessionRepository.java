26963ce93eaf49af0a7f65c1234bd02c90c253e5
Adrian Antkowiak <adiantek@gmail.com>
2022-05-28 15:22:30 +0200

    OrderBy & removing old sessions

--- src/main/java/com/workflow/workflow/user/UserSessionRepository.java
+++ src/main/java/com/workflow/workflow/user/UserSessionRepository.java
@@ -3,9 +3,12 @@ package com.workflow.workflow.user;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.OrderBy;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserSessionRepository extends CrudRepository<UserSession, Long> {
     Optional<UserSession> findBySession(String session);
+    @OrderBy("last_used DESC")
     List<UserSession> findByUser(User user);
 }
