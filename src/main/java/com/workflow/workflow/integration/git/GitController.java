9576040a9f9a3ca8664fe8b9f3c5c4304de7f3f6
SamPanDonte <bartosz.wawrzyniak.2000@gmail.com>
2022-05-29 15:04:50 +0200

    WFLOW-344 - move endpoints and deprecate old ones

--- src/main/java/com/workflow/workflow/integration/git/GitController.java
+++ src/main/java/com/workflow/workflow/integration/git/GitController.java
@@ -56,7 +56,7 @@ public class GitController {
     @ApiResponse(description = "Connection created.", responseCode = "200", content = @Content(schema = @Schema(implementation = Issue.class)))
     @ApiResponse(description = "No user logged in.", responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorType.class)))
     @ApiResponse(description = "Project or task or git issue not found.", responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorType.class)))
-    @PostMapping("/task/{taskId}/integration/git")
+    @PostMapping("/task/{taskId}/integration/git/issue")
     Mono<Issue> newIssue(@NotNull @Parameter(hidden = true) User user, @PathVariable long id,
             @PathVariable long taskId,
             @RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(required = false) Issue issue) {
@@ -75,11 +75,23 @@ public class GitController {
         return service.connectIssue(task, issue);
     }
 
+    @Deprecated
+    @Operation(summary = "Create git issue connection to task", description = "Creates new git issue connection with task. If request body is empty creates new issue. Otherwise uses existing git issue.")
+    @ApiResponse(description = "Connection created.", responseCode = "200", content = @Content(schema = @Schema(implementation = Issue.class)))
+    @ApiResponse(description = "No user logged in.", responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorType.class)))
+    @ApiResponse(description = "Project or task or git issue not found.", responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorType.class)))
+    @PostMapping("/task/{taskId}/integration/git")
+    Mono<Issue> newIssueOld(@NotNull @Parameter(hidden = true) User user, @PathVariable long id,
+            @PathVariable long taskId,
+            @RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(required = false) Issue issue) {
+        return newIssue(user, id, taskId, issue);
+    }
+
     @Operation(summary = "Delete git issue connection to task", description = "Deletes git issue connection with task. It does not delete issue on git service nor it deletes task.")
     @ApiResponse(description = "Connection deleted.", responseCode = "200")
     @ApiResponse(description = "No user logged in.", responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorType.class)))
     @ApiResponse(description = "Project or task or git issue connection not found.", responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorType.class)))
-    @DeleteMapping("/task/{taskId}/integration/git")
+    @DeleteMapping("/task/{taskId}/integration/git/issue")
     void deleteIssue(@NotNull @Parameter(hidden = true) User user, @PathVariable long id,
             @PathVariable long taskId) {
         Project project = projectRepository.findByIdOrThrow(id);
@@ -92,6 +104,17 @@ public class GitController {
             throw new ObjectNotFoundException();
         }
         service.deleteIssue(task);
+    }
+
+    @Deprecated
+    @Operation(summary = "Delete git issue connection to task", description = "Deletes git issue connection with task. It does not delete issue on git service nor it deletes task.")
+    @ApiResponse(description = "Connection deleted.", responseCode = "200")
+    @ApiResponse(description = "No user logged in.", responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorType.class)))
+    @ApiResponse(description = "Project or task or git issue connection not found.", responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorType.class)))
+    @DeleteMapping("/task/{taskId}/integration/git")
+    void deleteIssueOld(@NotNull @Parameter(hidden = true) User user, @PathVariable long id,
+            @PathVariable long taskId) {
+        deleteIssue(user, id, taskId);
     }
 
     @Operation(summary = "Retrieve git pull requests for project", description = "Retrieves all pull requests from all integrated git services for project.")
