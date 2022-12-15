738bcbfb20291fb0630c1ce546c5efadd9ee7765
Adrian Antkowiak <adiantek@gmail.com>
2022-12-15 08:16:04 +0100

    WFLOW-714 Implement captcha (#28)
    * Implement captcha
    
    * support for automated captcha test

--- src/main/java/dev/vernite/vernite/user/auth/ChangePasswordRequest.java
+++ src/main/java/dev/vernite/vernite/user/auth/ChangePasswordRequest.java
@@ -27,26 +27,10 @@
 
 package dev.vernite.vernite.user.auth;
 
+import lombok.Data;
+
+@Data
 public class ChangePasswordRequest {
-
     private String oldPassword;
-
     private String newPassword;
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
-
 }
