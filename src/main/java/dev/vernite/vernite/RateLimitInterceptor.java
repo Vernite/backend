f2a3c23ef5e6496ce7fc2ec64a66f6b8c19f1c94
Adrian Antkowiak <adiantek@gmail.com>
2023-01-09 02:48:21 +0100

    rewrite ratelimit to support IP limit & user ID limit

--- src/main/java/dev/vernite/vernite/RateLimitInterceptor.java
+++ src/main/java/dev/vernite/vernite/RateLimitInterceptor.java
@@ -1,80 +1,145 @@
 package dev.vernite.vernite;
 
-import java.time.Duration;
+import java.util.ArrayDeque;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import dev.vernite.vernite.user.auth.AuthController;
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * Rate limit interceptor. Limits GET requests to 1000 per minute and
  * POST/PUT/DELETE to 100 per minute.
  */
+@Component
 public class RateLimitInterceptor implements HandlerInterceptor {
 
-    private static final int GET_LIMIT = 1000;
+    private static final long TIME_LIMIT = TimeUnit.MINUTES.toMillis(1);
+    private static final int READ_LIMIT = 1000;
+    private static final int WRITE_LIMIT = 100;
 
-    private static final int MODIFY_LIMIT = 100;
+    private static final Map<Long, ArrayDeque<Long>> userReadLimit = new ConcurrentHashMap<>();
+    private static final Map<Long, ArrayDeque<Long>> userWriteLimit = new ConcurrentHashMap<>();
+    private static final Map<String, ArrayDeque<Long>> ipReadLimit = new ConcurrentHashMap<>();
+    private static final Map<String, ArrayDeque<Long>> ipWriteLimit = new ConcurrentHashMap<>();
 
-    private final Map<String, Bucket> getLimit = new ConcurrentHashMap<>();
-
-    private final Map<String, Bucket> modifyLimit = new ConcurrentHashMap<>();
-
-    private Bucket getGetBucket(String apiKey) {
-        return getLimit.computeIfAbsent(apiKey, k -> Bucket.builder()
-                .addLimit(Bandwidth.classic(GET_LIMIT, Refill.greedy(GET_LIMIT, Duration.ofMinutes(1))))
-                .build());
+    private static boolean isWriteMethod(String method) {
+        switch (method) {
+            case "GET":
+            case "HEAD":
+            case "OPTIONS":
+                return false;
+            case "PUT":
+            case "PATCH":
+            case "DELETE":
+            case "POST":
+                return true;
+            default:
+                return true;
+        }
     }
 
-    private Bucket getModifyBucket(String apiKey) {
-        return modifyLimit.computeIfAbsent(apiKey, k -> Bucket.builder()
-                .addLimit(Bandwidth.classic(MODIFY_LIMIT, Refill.greedy(MODIFY_LIMIT, Duration.ofMinutes(1))))
-                .build());
-    }
-
-    static private String getApiKey(HttpServletRequest request) {
-        for (var cookies : request.getCookies()) {
-            if (cookies.getName().equals(AuthController.COOKIE_NAME)) {
-                return cookies.getValue();
+    private static <T> int increment(T key, Map<T, ArrayDeque<Long>> map, long time, int maxSize)
+            throws TooManyRequestsException {
+        ArrayDeque<Long> deque = map.get(key);
+        if (deque == null) {
+            // do not add empty ArrayDeque to map - thread safety
+            deque = new ArrayDeque<>();
+            deque.addLast(time);
+            map.put(key, deque);
+            return maxSize - 1;
+        } else {
+            synchronized (deque) {
+                while (!deque.isEmpty() && time - deque.getFirst() > TIME_LIMIT) {
+                    deque.removeFirst();
+                }
+                if (deque.size() >= maxSize) {
+                    throw new TooManyRequestsException(TIME_LIMIT + deque.getFirst() - time);
+                }
+                deque.addLast(time);
+                map.putIfAbsent(key, deque);
+                return maxSize - deque.size();
             }
         }
-        return null;
     }
 
+    private static String getIP(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    // this interceptor is called before and after UserResolver
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        String apiKey = getApiKey(request);
-        if (apiKey == null) {
-            apiKey = request.getHeader("X-Forwarded-For");
-            if (apiKey == null) {
-                apiKey = request.getRemoteAddr();
+        boolean writeMethod = isWriteMethod(request.getMethod());
+        long now = System.currentTimeMillis();
+        try {
+            int remaining;
+
+            if (request.getAttribute("ratelimit") != null) {
+                // after user resolver:
+                Long userID = (Long) request.getAttribute("userID");
+                if (userID == null) {
+                    return true;
+                }
+                remaining = writeMethod ? increment(userID, userWriteLimit, now, WRITE_LIMIT)
+                        : increment(userID, userReadLimit, now, READ_LIMIT);
+                remaining = Math.min((int) request.getAttribute("ratelimit"), remaining);
+            } else {
+                // before user resolver:
+                String ip = getIP(request);
+                remaining = writeMethod ? increment(ip, ipWriteLimit, now, WRITE_LIMIT)
+                        : increment(ip, ipReadLimit, now, READ_LIMIT);
+                request.setAttribute("ratelimit", remaining);
             }
-        }
-
-        Bucket tokenBucket;
-        if (request.getMethod().equals("GET")) {
-            tokenBucket = getGetBucket(apiKey);
-        } else {
-            tokenBucket = getModifyBucket(apiKey);
-        }
-
-        var probe = tokenBucket.tryConsumeAndReturnRemaining(1);
-        if (probe.isConsumed()) {
-            response.addHeader("X-Rate-Limit-Remaining", String.valueOf(probe.getRemainingTokens()));
+            response.setHeader("X-Rate-Limit-Remaining", Integer.toString(remaining));
             return true;
-        } else {
-            long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
-            response.addHeader("X-Rate-Limit-Retry-After-Seconds", String.valueOf(waitForRefill));
+        } catch (TooManyRequestsException e) {
+            // round up
+            long seconds = (e.retryAfter + 999L) / 1000L;
+            response.setHeader("X-Rate-Limit-Retry-After-Seconds", Long.toString(seconds));
             response.sendError(429, "You have exhausted your API Request Quota");
             return false;
         }
+    }
+
+    private static <T> void clearMap(Map<T, ArrayDeque<Long>> map, long now) {
+        Iterator<ArrayDeque<Long>> it = map.values().iterator();
+        while (it.hasNext()) {
+            ArrayDeque<Long> deque = it.next();
+            while (!deque.isEmpty() && now - deque.getFirst() > TIME_LIMIT) {
+                deque.removeFirst();
+            }
+            if (deque.isEmpty()) {
+                it.remove();
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    public void cleanDeques() {
+        long now = System.currentTimeMillis();
+        clearMap(ipWriteLimit, now);
+        clearMap(ipReadLimit, now);
+        clearMap(userWriteLimit, now);
+        clearMap(userReadLimit, now);
+    }
+
+    @AllArgsConstructor
+    private static class TooManyRequestsException extends Exception {
+        @Getter
+        private long retryAfter;
     }
 }
